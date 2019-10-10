8844b8a24e2198f315ce870df3f068b429343901
saarthack-sachdeva <ssachdeva316@gmail.com>
2019-10-09 18:55:20 -0700

    Initial Commit for Adding Machine

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,64 @@
+	/*
+	 * Name: Saarthack Sachdeva
+	 * Classid: CSE 360 (Wed) - 70642
+	 * Assignment_Number: 2
+	 * Description: This is a calculator machine which has blank methods
+	 * 				This is an initial version, which would then be updated
+	 * 				after when we write the functions of the methods.
+	 */
+
+ package cse360assign2;
+
+	/**
+	 * Initial version of adding machine class which would
+	 * then be updated to perform the required functions.
+	 * @author saarthacksachdeva
+	 *
+	 */
+	public class AddingMachine {
+		private int total;
+
+		/**
+		 * this is a constructor which initializes the total value to be 0.
+		 */
+		public AddingMachine () {
+			total = 0;  // not needed - included for clarity}
+		}
+
+		/**
+		 * initially empty total method
+		 * @return an integer
+		 */
+		public int getTotal () {
+			return 0;
+		}
+
+		/**
+		 * initially empty add method
+		 * @param value
+		 */
+		public void add(int value) {
+
+		}
+
+		/**
+		 * initially empty subtract method
+		 * @param value
+		 */
+		public void subtract(int value) {
+
+		}
+
+		/**
+		 * initial empty method
+		 * @return String
+		 */
+		public String toString() {
+			return "";
+		}
+
+
+		public void clear() {
+
+		}
+	}
